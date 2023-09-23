d0252af513a3bc7311e54e8a94b4bf9be2e43d6f
eamonnmg <mcgonigle.eamonn@gmail.com>
2023-09-23 18:46:18 +0100

    use d3 scale and absolute position for day view

--- src/components/DayView/DayView.vue
+++ src/components/DayView/DayView.vue
@@ -2,6 +2,9 @@
 import { computed, onMounted, ref } from "vue";
 import type { TimeBlock } from "@/types";
 import HourDividerLines from "@/components/DayView/HourDividerLines.vue";
+import { scaleTime } from "d3";
+import { endOfToday, startOfToday } from "date-fns";
+import { useElementSize } from "@vueuse/core";
 
 interface Props {
   timeBlocks: TimeBlock[];
@@ -42,26 +45,54 @@ interface DayViewTimeBlock extends TimeBlock {
 const container = ref(null);
 const containerNav = ref(null);
 const containerOffset = ref(null);
+const nowLine = ref(null);
 
 const startGridOffsetRows = 3;
 const rows = 288 + startGridOffsetRows; // break 24 hour day into 288 5 minute segments
 
+// function timeBlockToDayViewTimeBlock(timeBlock: TimeBlock): DayViewTimeBlock {
+//   const startHour = new Date(timeBlock.start).getHours();
+//   const startMinute = new Date(timeBlock.start).getMinutes();
+//   const endHour = new Date(timeBlock.end).getHours();
+//   const endMinute = new Date(timeBlock.end).getMinutes();
+//
+//   const startMin = startHour * 60 + startMinute;
+//   const endMin = endHour * 60 + endMinute;
+//
+//   const startRow = Math.round(startMin / 5);
+//   const duration = Math.round((endMin - startMin) / 5);
+//
+//   return {
+//     ...timeBlock,
+//     row: startRow + startGridOffsetRows,
+//     span: duration,
+//     startTimeLabel: new Date(timeBlock.start).toLocaleTimeString([], {
+//       hour: "2-digit",
+//       minute: "2-digit",
+//     }),
+//   };
+// }
+
 function timeBlockToDayViewTimeBlock(timeBlock: TimeBlock): DayViewTimeBlock {
-  const startHour = new Date(timeBlock.start).getHours();
-  const startMinute = new Date(timeBlock.start).getMinutes();
-  const endHour = new Date(timeBlock.end).getHours();
-  const endMinute = new Date(timeBlock.end).getMinutes();
+  // const startHour = new Date(timeBlock.start).getHours();
+  // const startMinute = new Date(timeBlock.start).getMinutes();
+  // const endHour = new Date(timeBlock.end).getHours();
+  // const endMinute = new Date(timeBlock.end).getMinutes();
+  //
+  // const startMin = startHour * 60 + startMinute;
+  // const endMin = endHour * 60 + endMinute;
+  //
+  // const startRow = Math.round(startMin / 5);
+  // const duration = Math.round((endMin - startMin) / 5);
 
-  const startMin = startHour * 60 + startMinute;
-  const endMin = endHour * 60 + endMinute;
-
-  const startRow = Math.round(startMin / 5);
-  const duration = Math.round((endMin - startMin) / 5);
+  console.log(timeScale.value(new Date()));
 
   return {
     ...timeBlock,
-    row: startRow + startGridOffsetRows,
-    span: duration,
+    y: timeScale.value(new Date(timeBlock.start)),
+    height:
+      timeScale.value(new Date(timeBlock.end)) -
+      timeScale.value(new Date(timeBlock.start)),
     startTimeLabel: new Date(timeBlock.start).toLocaleTimeString([], {
       hour: "2-digit",
       minute: "2-digit",
@@ -74,19 +105,25 @@ const dayViewTimeBlocks = computed(() => {
 });
 
 onMounted(() => {
-  // Set the container scroll position based on the current time.
-  const currentMinute = new Date().getHours() * 60;
-  container.value.scrollTop =
-    ((container.value.scrollHeight -
-      containerNav.value.offsetHeight -
-      containerOffset.value.offsetHeight) *
-      currentMinute) /
-    1440;
+  nowLine.value.scrollIntoView({ block: "center" });
+});
+
+// const scrollHeight = computed(() => {
+//   if (!container.value) {
+//     return 0;
+//   }
+//   return container.value?.clientHeight;
+// });
+
+// const { height } = useElementSize(container);
+
+const timeScale = computed(() => {
+  return scaleTime().domain([startOfToday(), endOfToday()]).range([0, 2800]);
 });
 </script>
 
 <template>
-  <div ref="container" class="flex flex-auto flex-col overflow-auto">
+  <div ref="container" class="flex test flex-auto flex-col overflow-auto">
     <div
       ref="containerNav"
       class="sticky top-0 z-10 grid flex-none grid-cols-7 bg-white text-xs text-gray-500 shadow ring-1 ring-black ring-opacity-5 md:hidden"
@@ -144,24 +181,34 @@ onMounted(() => {
     </div>
     <div class="flex w-full flex-auto">
       <div class="w-14 flex-none bg-white ring-1 ring-gray-100" />
-      <div class="grid flex-auto grid-cols-1 grid-rows-1">
-        <!-- Horizontal lines -->
-        <HourDividerLines>
+      <!--      <div class="grid flex-auto grid-cols-1 grid-rows-1">-->
+      <!-- Horizontal lines -->
+
+      <div class="relative w-full h-full">
+        <HourDividerLines :time-scale="timeScale">
           <template #offset>
             <div ref="containerOffset" class="row-end-1 h-7"></div>
           </template>
         </HourDividerLines>
 
+        <div
+          ref="nowLine"
+          class="absolute h-[1px] w-full bg-red-500"
+          :style="{
+            transform: `translateY(${timeScale(new Date())}px)`,
+          }"
+        ></div>
+
         <!-- Events -->
-        <ol
-          class="col-start-1 col-end-2 row-start-1 grid grid-cols-1"
-          :style="`grid-template-rows: repeat(${rows}, minmax(0, 1fr)) auto`"
-        >
+        <ol class="relative w-full">
           <li
             v-for="timeBlock in dayViewTimeBlocks"
             :key="timeBlock.name"
-            class="relative mt-px flex"
-            :style="`grid-row: ${timeBlock.row} / span ${timeBlock.span}; grid-column: 1;`"
+            class="absolute left-0 w-full right-0 top-0 bottom-0 mt-px flex"
+            :style="{
+              height: `${timeBlock.height}px`,
+              transform: `translateY(${timeBlock.y}px)`,
+            }"
           >
             <a
               href="#"
@@ -179,6 +226,33 @@ onMounted(() => {
           </li>
         </ol>
       </div>
+
+      <!--        <ol-->
+      <!--          class="col-start-1 col-end-2 row-start-1 grid grid-cols-1"-->
+      <!--          :style="`grid-template-rows: repeat(${rows}, minmax(0, 1fr)) auto`"-->
+      <!--        >-->
+      <!--          <li-->
+      <!--            v-for="timeBlock in dayViewTimeBlocks"-->
+      <!--            :key="timeBlock.name"-->
+      <!--            class="relative mt-px flex"-->
+      <!--            :style="`grid-row: ${timeBlock.row} / span ${timeBlock.span}; grid-column: 1;`"-->
+      <!--          >-->
+      <!--            <a-->
+      <!--              href="#"-->
+      <!--              :class="`group absolute inset-1 flex flex-col overflow-y-auto rounded-lg ${timeBlock.color} p-2 text-xs leading-5`"-->
+      <!--            >-->
+      <!--              <p class="order-1 font-semibold text-blue-700">-->
+      <!--                {{ timeBlock.name }}-->
+      <!--              </p>-->
+      <!--              <p class="text-blue-500 group-hover:text-blue-700">-->
+      <!--                <time datetime="2022-01-22T06:00">{{-->
+      <!--                  timeBlock.startTimeLabel-->
+      <!--                }}</time>-->
+      <!--              </p>-->
+      <!--            </a>-->
+      <!--          </li>-->
+      <!--        </ol>-->
+      <!--      </div>-->
     </div>
   </div>
 </template>
--- src/components/DayView/HourDividerLines.vue
+++ src/components/DayView/HourDividerLines.vue
@@ -1,205 +1,258 @@
-<template>
-  <div
-    class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
-    style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))"
-  >
-    <slot name="offset"> </slot>
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        12AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        1AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        2AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        3AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        4AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        5AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        6AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        7AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        8AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        9AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        10AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        11AM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        12PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        1PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        2PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        3PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        4PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        5PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        6PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        7PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        8PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        9PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        10PM
-      </div>
-    </div>
-    <div />
-    <div>
-      <div
-        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-      >
-        11PM
-      </div>
-    </div>
-    <div />
-  </div>
-</template>
-<script>
-export default {
-  name: "HourDividerLines",
-};
+<script setup lang="ts">
+import { scaleTime } from "d3";
+import { endOfToday, startOfToday, add, format } from "date-fns";
+import { computed } from "vue";
+
+interface Props {
+  timeScale: any;
+}
+
+const props = defineProps<Props>();
+
+// const timeScale = scaleTime()
+//   .domain([startOfToday(), endOfToday()])
+//   .range([0, 1800]);
+
+const hourLabels = computed(() => {
+  return Array.from({ length: 24 }).map((div, i) => {
+    const dateTime = add(startOfToday(), { hours: i });
+    return {
+      dateTime,
+      label: format(dateTime, "h a"),
+      y: props.timeScale(dateTime),
+    };
+  });
+});
+
+const halfHourDividerLines = computed(() => {
+  return Array.from({ length: 48 }).map((div, i) => {
+    const dateTime = add(startOfToday(), { minutes: i * 30 });
+    return {
+      dateTime,
+      label: format(dateTime, "h a"),
+      y: props.timeScale(dateTime),
+    };
+  });
+});
+
+// console.log(hourDividerLines);
 </script>
+
+<template>
+  <div class="relative">
+    <div
+      v-for="divider in hourLabels"
+      :key="divider.dateTime"
+      class="absolute -left-10 -top-2 border-0 top-0 bottom-0 text-right text-xs leading-5 text-gray-400"
+      :style="{ transform: `translateY(${divider.y}px)` }"
+    >
+      {{ divider.label }}
+    </div>
+  </div>
+  <div class="relative w-full divide-y divide-gray-100">
+    <div
+      v-for="divider in halfHourDividerLines"
+      :key="divider.dateTime"
+      class="absolute left-0 w right-0 top-0 bottom-0"
+      :style="{ transform: `translateY(${divider.y}px)` }"
+    ></div>
+  </div>
+  <!--  <div-->
+  <!--    class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"-->
+  <!--    style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))"-->
+  <!--  >-->
+  <!--    <slot name="offset"> </slot>-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        12AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        1AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        2AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        3AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        4AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        5AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        6AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        7AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        8AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        9AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        10AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        11AM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        12PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        1PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        2PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        3PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        4PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        5PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        6PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        7PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        8PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        9PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        10PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--    <div>-->
+  <!--      <div-->
+  <!--        class="sticky left-0 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400"-->
+  <!--      >-->
+  <!--        11PM-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div />-->
+  <!--  </div>-->
+</template>
