6e2e1fad937bd93f0d8a9953847a77315619939f
eamonnmg <mcgonigle.eamonn@gmail.com>
2024-02-14 11:58:29 +0000

    tidy

--- src/Plan/DayView/DayView.vue
+++ src/Plan/DayView/DayView.vue
@@ -90,15 +90,6 @@ onMounted(() => {
   nowLine.value.scrollIntoView({ block: "center" });
 });
 
-// const scrollHeight = computed(() => {
-//   if (!container.value) {
-//     return 0;
-//   }
-//   return container.value?.clientHeight;
-// });
-
-// const { height } = useElementSize(container);
-
 const timeScale = computed(() => {
   console.log("timeScale", [startOfDay(props.day), endOfDay(props.day)]);
   return scaleTime()
@@ -116,9 +107,13 @@ function onTimelineClick(e) {
 
 function calcIdealTextColor(color: string) {
   if (!color) return "#000";
-  const contrast = chroma.contrast("#fff", color);
-  console.log("contrast", Math.abs(4.5 - contrast));
-  return Math.abs(4.5 - contrast) < 2 ? "white" : "black";
+  // chroma throws error if color is not valid
+  try {
+    const contrast = chroma.contrast("#fff", color);
+    return Math.abs(4.5 - contrast) < 2 ? "white" : "black";
+  } catch {
+    return "black";
+  }
 }
 </script>
 
@@ -131,58 +126,7 @@ function calcIdealTextColor(color: string) {
     <div
       ref="containerNav"
       class="sticky top-0 z-10 grid flex-none grid-cols-7 bg-white text-xs text-gray-500 shadow ring-1 ring-black ring-opacity-5 md:hidden"
-    >
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>W</span>-->
-      <!--        &lt;!&ndash; Default: "text-gray-900", Selected: "bg-gray-900 text-white", Today (Not Selected): "text-indigo-600", Today (Selected): "bg-indigo-600 text-white" &ndash;&gt;-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900"-->
-      <!--          >19</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>T</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-indigo-600"-->
-      <!--          >20</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>F</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900"-->
-      <!--          >21</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>S</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full bg-gray-900 text-base font-semibold text-white"-->
-      <!--          >22</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>S</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900"-->
-      <!--          >23</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>M</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900"-->
-      <!--          >24</span-->
-      <!--        >-->
-      <!--      </button>-->
-      <!--      <button type="button" class="flex flex-col items-center pb-1.5 pt-3">-->
-      <!--        <span>T</span>-->
-      <!--        <span-->
-      <!--          class="mt-3 flex h-8 w-8 items-center justify-center rounded-full text-base font-semibold text-gray-900"-->
-      <!--          >25</span-->
-      <!--        >-->
-      <!--      </button>-->
-    </div>
+    ></div>
     <div class="flex w-full flex-auto">
       <div class="w-14 flex-none bg-white ring-1 ring-gray-100" />
       <!--      <div class="grid flex-auto grid-cols-1 grid-rows-1">-->
@@ -245,33 +189,6 @@ function calcIdealTextColor(color: string) {
           </li>
         </ol>
       </div>
-
-      <!--        <ol-->
-      <!--          class="col-start-1 col-end-2 row-start-1 grid grid-cols-1"-->
-      <!--          :style="`grid-template-rows: repeat(${rows}, minmax(0, 1fr)) auto`"-->
-      <!--        >-->
-      <!--          <li-->
-      <!--            v-for="timeBlock in dayViewTimeBlocks"-->
-      <!--            :key="timeBlock.name"-->
-      <!--            class="relative mt-px flex"-->
-      <!--            :style="`grid-row: ${timeBlock.row} / span ${timeBlock.span}; grid-column: 1;`"-->
-      <!--          >-->
-      <!--            <a-->
-      <!--              href="#"-->
-      <!--              :class="`group absolute inset-1 flex flex-col overflow-y-auto rounded-lg ${timeBlock.color} p-2 text-xs leading-5`"-->
-      <!--            >-->
-      <!--              <p class="order-1 font-semibold text-blue-700">-->
-      <!--                {{ timeBlock.name }}-->
-      <!--              </p>-->
-      <!--              <p class="text-blue-500 group-hover:text-blue-700">-->
-      <!--                <time datetime="2022-01-22T06:00">{{-->
-      <!--                  timeBlock.startTimeLabel-->
-      <!--                }}</time>-->
-      <!--              </p>-->
-      <!--            </a>-->
-      <!--          </li>-->
-      <!--        </ol>-->
-      <!--      </div>-->
     </div>
   </div>
 </template>
